8e3997f0982c51d1b25a42f0f51cfeae26edeb5c
Giocodingtemple <gmarchiori5149@gmail.com>
2022-08-07 20:29:32 -0500

    why is it soo much pain flask is

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
-from pickle import FALSE
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = FALSE
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
